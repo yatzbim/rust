1eb1f285b8ecb2fdf8510bcaabd8f85d362b62bc
Jonah Miller <jonahmiller5@gmail.com>
2025-08-04 16:25:06 +0300

    finished assembly_line exercise

--- exercises/concept/assembly-line/src/lib.rs
+++ exercises/concept/assembly-line/src/lib.rs
@@ -1,7 +1,21 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    todo!("calculate hourly production rate at speed: {speed}")
+    let rate = if speed < 1 {
+        0.0
+    } else if speed < 5 {
+        1.0
+    } else if speed < 9 {
+        0.9
+    } else if speed < 11 {
+        0.77
+    } else {
+        panic!("{} is greater than maximum allowed speed of 10!", speed)
+    };
+
+    rate * speed as f64 * 221.0
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    todo!("calculate the amount of working items at speed: {speed}")
+    let rate = production_rate_per_hour(speed);
+    let items_per_minute = rate / 60.0;
+    items_per_minute as u32
 }
