d7f4695ca275b75151d386114b7e6e636f9f5946
Jonah Miller <jonahmiller5@gmail.com>
2025-08-06 12:50:53 +0300

    finished magazine-cutout

--- exercises/concept/magazine-cutout/src/lib.rs
+++ exercises/concept/magazine-cutout/src/lib.rs
@@ -5,5 +5,28 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    todo!()
+    let mut magazine_words = HashMap::new();
+    for word in magazine {
+        if let Some(count) = magazine_words.get(word) {
+            magazine_words.insert(word, count + 1);
+        } else {
+            magazine_words.insert(word, 1);
+        }
+    }
+
+    for word in note {
+        let v = magazine_words.get(word);
+        match v {
+            Some(count) => {
+                if *count == 1 {
+                    magazine_words.remove(word);
+                } else {
+                    magazine_words.insert(word, count - 1);
+                }
+            }
+            None => return false,
+        }
+    }
+
+    true
 }
