7fe0f396c939d0472234d78ce4f6455822f2d6b4
Jonah Miller <jonahmiller5@gmail.com>
2025-08-06 12:20:56 +0300

    finished low-power-embedded-game

--- exercises/concept/low-power-embedded-game/src/lib.rs
+++ exercises/concept/low-power-embedded-game/src/lib.rs
@@ -3,19 +3,38 @@
 #![allow(unused)]
 
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
-    todo!("implement `fn divmod`");
+    if divisor == 0 {
+        panic!("Cannot divide by zero!");
+    }
+
+    let is_negative = (dividend < 0 || divisor < 0) && !(dividend < 0 && divisor < 0);
+
+    let mut quotient = 0;
+    let mut remainder = dividend;
+    while remainder >= divisor {
+        remainder -= divisor;
+        quotient += 1;
+    }
+
+    (quotient, remainder)
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    todo!("implement `fn evens`");
     // TODO: remove this; it's only necessary to allow this function to compile
     // before the student has done any work.
-    std::iter::empty()
+
+    let mut i = 0;
+    iter.filter(move |v| {
+        let res = i % 2 == 0;
+        i = i + 1;
+        res
+    })
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        todo!("implement `fn manhattan`")
+        let Position(x, y) = self;
+        x.abs() + y.abs()
     }
 }
