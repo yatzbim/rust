400557b49c03e96c364aaad758aac159e87cccef
Jonah Miller <jonahmiller5@gmail.com>
2025-08-07 12:47:37 +0300

    finished rpm-calculator

--- exercises/concept/rpn-calculator/src/lib.rs
+++ exercises/concept/rpn-calculator/src/lib.rs
@@ -8,7 +8,35 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    todo!(
-        "Given the inputs: {inputs:?}, evaluate them as though they were a Reverse Polish notation expression"
-    );
+    let mut stack: Vec<i32> = Vec::new();
+    for input in inputs {
+        if let CalculatorInput::Value(v) = input {
+            stack.push(*v);
+        } else {
+            let v2 = stack.pop();
+            let v1 = stack.pop();
+            if v1.is_none() || v2.is_none() {
+                return None;
+            } else {
+                let v2 = v2.unwrap();
+                let v1 = v1.unwrap();
+                match input {
+                    CalculatorInput::Add => {
+                        stack.push(v1 + v2);
+                    }
+                    CalculatorInput::Subtract => {
+                        stack.push(v1 - v2);
+                    }
+                    CalculatorInput::Multiply => {
+                        stack.push(v1 * v2);
+                    }
+                    CalculatorInput::Divide => {
+                        stack.push(v1 / v2);
+                    }
+                    _ => (),
+                }
+            }
+        }
+    }
+    if stack.len() == 1 { stack.pop() } else { None }
 }
