1375eb8c3de4069623dda1c9fbb02595a9d91536
Jonah Miller <jonahmiller5@gmail.com>
2025-08-07 12:18:04 +0300

    finished role-playing-game

--- exercises/concept/role-playing-game/src/lib.rs
+++ exercises/concept/role-playing-game/src/lib.rs
@@ -6,10 +6,41 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        todo!("Revive this player")
+        if self.health == 0 {
+            let mana = if self.level >= 10 { Some(100) } else { None };
+            Some(Player {
+                health: 100,
+                mana,
+                level: self.level,
+            })
+        } else {
+            None
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        todo!("Cast a spell of cost {mana_cost}")
+        match self.mana {
+            Some(mana) => {
+                if mana_cost > mana {
+                    return 0;
+                }
+
+                let mana_remaining = mana - mana_cost;
+                self.mana = if mana_remaining > 0 {
+                    Some(mana_remaining)
+                } else {
+                    None
+                };
+                mana_cost * 2
+            }
+            None => {
+                if self.health < mana_cost {
+                    self.health = 0;
+                } else {
+                    self.health -= mana_cost;
+                }
+                0
+            }
+        }
     }
 }
