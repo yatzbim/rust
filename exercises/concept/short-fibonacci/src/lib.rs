24a394d4b85972c1ceffa7e5c6e6c3740d986c63
Jonah Miller <jonahmiller5@gmail.com>
2025-08-07 13:56:12 +0300

    finished short-fibonacci

--- exercises/concept/short-fibonacci/src/lib.rs
+++ exercises/concept/short-fibonacci/src/lib.rs
@@ -1,13 +1,15 @@
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    todo!()
+    create_buffer(0)
 }
 
 /// Create a buffer of `count` zeroes.
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    todo!("create a zeroized buffer of {count} bytes")
+    let mut buffer = vec![0; count];
+    buffer.truncate(count);
+    buffer
 }
 
 /// Create a vector containing the first five elements of the Fibonacci sequence.
@@ -15,5 +17,22 @@ pub fn create_buffer(count: usize) -> Vec<u8> {
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    todo!()
+    let mut buffer = create_buffer(5);
+    fibonacci_helper(&mut buffer, 0);
+    buffer
+}
+
+fn fibonacci_helper(buffer: &mut Vec<u8>, index: usize) {
+    if index == 0 {
+        buffer[0] = 1;
+        buffer[1] = 1;
+        fibonacci_helper(buffer, 2);
+    } else if index >= buffer.len() {
+        return;
+    } else {
+        let a = buffer.get(index - 2).unwrap();
+        let b = buffer.get(index - 1).unwrap();
+        buffer[index] = a + b;
+        fibonacci_helper(buffer, index + 1);
+    }
 }
