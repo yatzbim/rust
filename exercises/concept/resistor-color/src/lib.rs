e0f6c5e5e74bf072303e74e2d715a1a74b501fe1
Jonah Miller <jonahmiller5@gmail.com>
2025-08-07 11:48:51 +0300

    finished resistor-color

--- exercises/concept/resistor-color/src/lib.rs
+++ exercises/concept/resistor-color/src/lib.rs
@@ -13,13 +13,47 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    todo!("convert color {color:?} into a numerical representation")
+    match color {
+        ResistorColor::Black => 0,
+        ResistorColor::Blue => 6,
+        ResistorColor::Brown => 1,
+        ResistorColor::Green => 5,
+        ResistorColor::Grey => 8,
+        ResistorColor::Orange => 3,
+        ResistorColor::Red => 2,
+        ResistorColor::Violet => 7,
+        ResistorColor::White => 9,
+        ResistorColor::Yellow => 4,
+    }
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    todo!("convert the value {value} into a string representation of color")
+    match value {
+        0 => String::from("Black"),
+        1 => String::from("Brown"),
+        2 => String::from("Red"),
+        3 => String::from("Orange"),
+        4 => String::from("Yellow"),
+        5 => String::from("Green"),
+        6 => String::from("Blue"),
+        7 => String::from("Violet"),
+        8 => String::from("Grey"),
+        9 => String::from("White"),
+        _ => String::from("value out of range"),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    todo!("return a list of all the colors ordered by resistance")
+    Vec::from([
+        ResistorColor::Black,
+        ResistorColor::Brown,
+        ResistorColor::Red,
+        ResistorColor::Orange,
+        ResistorColor::Yellow,
+        ResistorColor::Green,
+        ResistorColor::Blue,
+        ResistorColor::Violet,
+        ResistorColor::Grey,
+        ResistorColor::White,
+    ])
 }
